0c7cf99edfceaa375e0eb533bb0b8b52bd63e43b
Robin Avery <robin@ribbanya.dev>
2022-07-28 07:17:46 -0400

    temp commit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import asyncio
+import os
 import platform
+import re
 from functools import reduce
+from typing import Union, List
+
 import aiohttp
 import json
 
@@ -10,6 +14,8 @@ if platform.system() == 'Windows':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 DISCORD_API_INVITE_URL = "https://discordapp.com/api/invite/"
+
+INVITE_RE = re.compile(r"(?:(?:https?\:\/\/)?discord\.gg\/)?([\da-zA-Z]+)")
 
 
 def deep_get(dictionary, *keys, default=None):
@@ -23,9 +29,28 @@ async def get_invite_name(session: ClientSession, invite_code: str) -> str:
         return deep_get(json_data, "guild", "name")
 
 
+async def parse_smashcords(acc: dict, path: Union[str, bytes, os.PathLike]):
+    root: List[dict]
+    with open(path, "r") as f:
+        root = json.load(f)
+    for category in root:
+        category_name = category.get("name")
+        category_type = category.get("type")
+
+        for server in category.get("Servers", []):
+            server_name = server.get("name")
+            invite_url = server.get("destination")
+            invite_code = None if not invite_url else INVITE_RE.match(invite_url).group(1)
+            print({
+                "category": category_name,
+                "name": server_name,
+                "invite_code": invite_code,
+            })
+
+
 async def main():
     async with aiohttp.ClientSession() as session:
-        print(await get_invite_name(session, "4jpjZkB"))
+        await parse_smashcords({}, r"data/smashcords.json")
 
 
 if __name__ == '__main__':
